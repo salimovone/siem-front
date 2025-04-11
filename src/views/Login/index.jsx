7e58e4dcf4bd24d4b09397049493ace7b24d5cdd
salimovone <salimovone@gmail.com>
2025-04-11 13:45:09 +0500

    login page added

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,41 +1,50 @@
-import { Box, CssBaseline, FormControl, FormLabel, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, FormControl, FormControlLabel, Stack, TextField, Typography } from "@mui/material";
 import React from "react";
 
-const Login = () => {
+const SignIn = () => {
     return (
-        <Box sx={{ height: "100vh", width: "100vw", pt: "8%" }}>
-            <Grid container>
-                <CssBaseline />
-                <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    sx={{
-                        paddingInline: "20px",
-                        // width: "100%",
-                        margin: "auto",
-                    }}
-                >
-                    <Box
-                        sx={{
-                            width: "100%",
-                            maxWidth: "800px",
-                            minWidth: "500px",
-                            padding: 3,
-                        }}
-                    >
-                        <FormControl fullWidth>
-                            <Typography variant="h5" sx={{ mb: 2 }}>
-                                Sign in
-                            </Typography>
-                            <TextField label="Email" required type="email" sx={{maxWidth: "400px", minWidth: "250px", width: "100%"}}/>
-                        </FormControl>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Box>
+        <Stack
+            direction={"row"}
+            mt={20}
+            justifyContent={"center"}
+        >
+            <Box sx={{ width: "100%", maxWidth: "350px", boxShadow: 3, borderRadius: 2, p: 4, pt: 5 }}>
+                <Typography variant="h5" fontWeight={600} textAlign={"center"}>
+                    Sign In
+                </Typography>
+                <Typography variant="body2" textAlign={"center"} mt={1} color="#0009">
+                    Welcome user, please sign in to continue
+                </Typography>
+                <Box>
+                    <FormControl fullWidth>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            fullWidth
+                            sx={{ mt: 2, width: "100%"}}
+                            type="email"
+                            size="small"
+                        />
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                            sx={{ mt: 2 }}
+                            type="password"
+                            size="small"
+                        />
+
+                        <Stack>
+                            <FormControlLabel control={<Checkbox size="small" />} label="Remember me" sx={{ mt: 2, paddingLeft: "2px", color: "#0009" }} />
+                        </Stack>
+                        
+                        <Button variant="contained" sx={{my: 3}} >Sign In</Button>
+
+                    </FormControl>
+                </Box>
+            </Box>
+        </Stack>
     );
 };
 
-export default Login;
+export default SignIn;
